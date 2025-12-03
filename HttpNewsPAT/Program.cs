106bdd772ba75fc3302493aba81bdcc4f1f90577
sosiska2000 <maratoleg2006@gmail.com>
2025-12-03 21:30:25 +0500

    еще раз написал

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }

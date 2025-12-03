0cef97acba51af1434c3143f3d5ceb36cda25c62
sosiska2000 <maratoleg2006@gmail.com>
2025-12-03 17:30:13 +0500

    Добавьте файлы проекта.

---
+++ HttpNewsPAT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpNewsPAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

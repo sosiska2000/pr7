ba56e1481d4194f95ed97f7be6aa2b7c8724b126
sosiska2000 <maratoleg2006@gmail.com>
2025-12-04 00:07:40 +0500

    Получение информации с сайта.Komornyi

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -12,17 +12,6 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("http://news.permaviat.ru/main");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Console.WriteLine(response.StatusDescription);
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-            Console.Read();
         }
     }
 }

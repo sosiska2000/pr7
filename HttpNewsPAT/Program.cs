782fef1a5aef54367f533c35511d8d46e5ee6cb5
ANosok <kirill60278@gmail.com>
2025-12-10 16:25:28 +0500

    redact from ao

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -12,6 +12,19 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
+            WebRequest Request = WebRequest.Create("http://news.permaviat.ru/main");
+            using (HttpWebResponse Response = (HttpWebResponse)Request.GetResponse()){
+                Console.WriteLine(Response.StatusDescription);
+
+                using (Stream DataStream = Response.GetResponseStream()) {
+                    using (StreamReader Reader = new StreamReader(DataStream))
+                    {
+                        string ResponseFromServer = Reader.ReadToEnd();
+                        Console.WriteLine(ResponseFromServer);
+                    }
+                }
+            }
+            Console.Read();
         }
     }
 }
